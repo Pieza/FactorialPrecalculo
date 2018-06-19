7eb0f8c11389e799f701bbc294a4f5716913b648
Pieza <jandres258@hotmail.com>
2018-06-18 21:46:41 -0600

    Add Vue JS and Semantic Ui
    Create the base for the project ;)
    (Es vara poner una descripción clara)

---
+++ js/vueScript.js
@@ -0,0 +1,11 @@
+/* Create a Vue instance
+    Here we create the module app
+*/
+new Vue({
+    template: '#app',
+    data: function() {
+        return {
+            number: null
+        }
+    }
+}).$mount('#app'); 
